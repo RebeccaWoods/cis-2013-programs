eade386c24a07a323e126c889d264b82e677beb7
RebeccaWoods <122952767+RebeccaWoods@users.noreply.github.com>
2023-02-10 13:00:54 -0600

    Add files via upload

---
+++ HandsOn3.js
@@ -0,0 +1,10 @@
+var floatAge, floatDays, intWeeks, floatMonths, intFortnight;
+/*This program will calculate age in days, weeks, months, and fortnights*/
+floatAge= parseFloat(prompt("How old are you?"))
+floatDays= parseFloat(floatAge*365.25)
+intWeeks=parseInt(floatDays/7)
+floatMonths=parseFloat(floatAge*12)
+intFortnight=parseInt(floatDays/14)
+alert("Do you want to see something cool?")
+alert("You are "+floatAge+" years old!"+"\n"+"You are "+floatDays+" days old!"+"\n"+"You are "+intWeeks+" weeks old!"+"\n"+"You are "+floatMonths+" months old!"+"\n"+"And finally, you are "+intFortnight+" in terms of fortnights!")
+alert("Ta-da!! Have a great day!")
